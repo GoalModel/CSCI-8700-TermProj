59c2af1f32dca93ad90b9b998a8604fa7fce81b0
Michael Brandon <mbbrandon@unomaha.edu>
2017-10-19 10:12:35 +0000

    Very minimally viable product.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :matters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'matters#index'
+  get 'matters/index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+    resources :matters
+
 
   # Example resource route with options:
   #   resources :products do
